dcebf397976c4298444aed4cc1b48c2372962fad
Gustavo <142137414+Ghosthszz@users.noreply.github.com>
2024-02-18 19:32:22 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,63 @@
+function adicionarUsuario(username, email) {
+    var githubToken = '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';
+    var owner = 'Gustavo';
+    var repo = 'criar';
+    var filePath = 'usuarios.json';
+
+    // Construa a URL da API do GitHub para o arquivo específico
+    var apiUrl = `https://api.github.com/repos/${owner}/${repo}/contents/${filePath}`;
+
+    // Configuração da requisição para obter o conteúdo do arquivo
+    var requestOptions = {
+        method: 'GET',
+        headers: {
+            'Authorization': 'token ' + githubToken,
+            'User-Agent': 'Ghosthszz'
+        }
+    };
+
+    // Faça uma requisição GET para obter o conteúdo atual do arquivo
+    fetch(apiUrl, requestOptions)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erro ao obter conteúdo do arquivo');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Decodifique o conteúdo do arquivo
+            var fileContent = window.atob(data.content);
+            // Converta o conteúdo em um objeto JavaScript
+            var users = JSON.parse(fileContent);
+            // Adicione o novo usuário
+            users.push({ username: username, email: email });
+
+            // Configuração da requisição para atualizar o arquivo
+            var updateOptions = {
+                method: 'PUT',
+                headers: {
+                    'Authorization': 'token ' + githubToken,
+                    'Content-Type': 'application/json',
+                    'User-Agent': 'Nome de usuário no GitHub'
+                },
+                body: JSON.stringify({
+                    message: 'Adicionando novo usuário',
+                    content: window.btoa(JSON.stringify(users)),
+                    sha: data.sha
+                })
+            };
+
+            // Faça uma requisição PUT para atualizar o arquivo
+            return fetch(apiUrl, updateOptions);
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erro ao atualizar arquivo');
+            }
+            alert('Usuário adicionado com sucesso!');
+        })
+        .catch(error => {
+            console.error(error);
+            alert('Erro ao adicionar usuário');
+        });
+}
