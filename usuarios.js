84111a01669e075e637d43d3958c45171891b198
Gustavo <142137414+Ghosthszz@users.noreply.github.com>
2024-02-19 19:36:19 -0300

    Adicionando novo usuário

--- usuarios.js
+++ usuarios.js
@@ -1,8 +1,15 @@
 
-const usuários = WyJbXVxuIix7InVzZXJuYW1lIjoieHMiLCJlbWFpbCI6ImlmQGdtYWlsLmNv
-bSJ9XQ==
+const usuários = CmNvbnN0IHVzdcOhcmlvcyA9IFd5SmJYVnh1SWl4N0luVnpaWEp1WVcxbElq
+b2llSE1pTENKbGJXRnBiQ0k2SW1sbVFHZHRZV2xzTG1OdgpiU0o5WFE9PQo7
+CmNvbnN0IG5vdm9Vc3XDoXJpbyA9IHsgdXN1w6FyaW86ICJnaG9zdGhzenpf
+Iiwgc2VuaGE6ICJndUBnbWFpbC5jb20iIH07CnVzdcOhcmlvcy5wdXNoKG5v
+dm9Vc3XDoXJpbyk7CgpmdW5jdGlvbiB2ZXJpZmljYXJDcmVkZW5jaWFpcyh1
+c3VhcmlvLCBzZW5oYSkgewogICAgY29uc3QgZm91bmRVc2VyID0gdXN1w6Fy
+aW9zLmZpbmQodSA9PiB1LnVzdcOhcmlvID09PSB1c3VhcmlvICYmIHUuc2Vu
+aGEgPT09IHNlbmhhKTsKICAgIHJldHVybiBmb3VuZFVzZXIgfHwgbnVsbDsK
+fQo=
 ;
-const novoUsuário = { usuário: "ghosthszz_", senha: "gu@gmail.com" };
+const novoUsuário = { usuário: "df", senha: "se" };
 usuários.push(novoUsuário);
 
 function verificarCredenciais(usuario, senha) {
