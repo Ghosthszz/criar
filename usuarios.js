00cbd25e8159e46529fb69ecc42f99ee4af57d1d
Gustavo <142137414+Ghosthszz@users.noreply.github.com>
2024-02-19 20:23:34 -0300

    Update usuarios.js

--- usuarios.js
+++ usuarios.js
@@ -1,10 +1,10 @@
-const  usuÃƒÆ’Ã‚Â¡rios  =  [
-  
-{"usuario": "user1", "senha": "senha1"},
+const usuários = [
+    {"usuario": "user1", "senha": "senha1"},
     {"usuario": "user2", "senha": "senha2"},
-    {"usuario": "user3", "senha": "senha3"},
+    {"usuario": "user3", "senha": "senha3"}
+];
 
-function verificarCredenciais(usuário, senha) {
-    const foundUser = usuários.find(u => u.usuário === usuário && u.senha === senha);
+function verificarCredenciais(usuario, senha) {
+    const foundUser = usuários.find(u => u.usuario === usuario && u.senha === senha);
     return foundUser || null;
 }
