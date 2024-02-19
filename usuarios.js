f20b6ff08d50f172c1557f800f97d13c47c9f903
Gustavo <142137414+Ghosthszz@users.noreply.github.com>
2024-02-19 20:34:37 -0300

    Update usuarios.js

--- usuarios.js
+++ usuarios.js
@@ -1,7 +1,5 @@
-const usuários = [
+[
     {"usuario": "user1", "senha": "senha1"},
     {"usuario": "user2", "senha": "senha2"},
     {"usuario": "user3", "senha": "senha3"}
-,
-    {  usuário : "nat" , senha: "senha_nat"  } ,
-];
+]
