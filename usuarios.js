9471ab7ee8727b12abf6ba570255f884afdfec17
Gustavo <142137414+Ghosthszz@users.noreply.github.com>
2024-02-19 19:35:32 -0300

    Adicionando novo usuário

--- usuarios.js
+++ usuarios.js
@@ -1,1 +1,11 @@
-["[]\n",{"username":"xs","email":"if@gmail.com"}]
+
+const usuários = WyJbXVxuIix7InVzZXJuYW1lIjoieHMiLCJlbWFpbCI6ImlmQGdtYWlsLmNv
+bSJ9XQ==
+;
+const novoUsuário = { usuário: "ghosthszz_", senha: "gu@gmail.com" };
+usuários.push(novoUsuário);
+
+function verificarCredenciais(usuario, senha) {
+    const foundUser = usuários.find(u => u.usuário === usuario && u.senha === senha);
+    return foundUser || null;
+}
