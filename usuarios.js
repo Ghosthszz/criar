828b4d9e8831a2eed7122a41f2539f51bd2cbf48
Gustavo <142137414+Ghosthszz@users.noreply.github.com>
2024-02-20 09:00:50 -0300

    Adicionando novo usuário

--- usuarios.js
+++ usuarios.js
@@ -2,4 +2,6 @@
     {"usuario": "user1", "senha": "senha1"},
     {"usuario": "user2", "senha": "senha2"},
     {"usuario": "user3", "senha": "senha3"}
-]
+,
+    {  usuário : "Opa" , senha: "oi"  } ,
+];
